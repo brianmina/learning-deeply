1ba8b142ec143128a9ba6b78c0cdcb6a5b11d270
brian <brian.mina17@gmail.com>
2024-01-21 17:29:43 -0500

    using two functions for doing 1 job

--- functions/send_messages.py
+++ functions/send_messages.py
@@ -1,20 +1,25 @@
 def show_messages(message):
     """Prints each message in the list."""
+    print("showing all the messages:")
     for msg in message:
         print(msg)
+
+
+def send_messages(messages, sent_messages):
+    """Prints each message and move then to a new list."""
+    print("\nSending each message:")
+    while messages:
+        each_message = messages.pop()
+        print(each_message)
+        sent_messages.append(each_message)
+
 
 messages = ["today is a good day.", "trust in yourself", "live the moment"]
 show_messages(messages)
 
+sent_messages = []
+send_messages(messages, sent_messages)
 
-sent_message = []
-
-
-def send_messages(message, sent_mesages):
-    """Prints each message and move then to a new list."""
-    while send_message
-
-
-new_list = send_messages(message, sent_messages)
-
-
+print("\nfinal list:")
+print(messages)
+print(sent_messages)
