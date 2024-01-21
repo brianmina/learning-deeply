e664e7534d79649f79510108677ba27d944547be
brian <brian.mina17@gmail.com>
2024-01-20 20:10:30 -0500

    excersicing with functions

---
+++ functions/message.py
@@ -0,0 +1,9 @@
+
+
+def show_messages(message):
+    """Prints each message in the list."""
+    for msg in message:
+        print(msg)
+
+messages = ["today is a good day.", "trust in yourself", "live the moment"]
+show_messages(messages)
---
+++ functions/send_messages.py
@@ -0,0 +1,20 @@
+def show_messages(message):
+    """Prints each message in the list."""
+    for msg in message:
+        print(msg)
+
+messages = ["today is a good day.", "trust in yourself", "live the moment"]
+show_messages(messages)
+
+
+sent_message = []
+
+
+def send_messages(message, sent_mesages):
+    """Prints each message and move then to a new list."""
+    while send_message
+
+
+new_list = send_messages(message, sent_messages)
+
+
