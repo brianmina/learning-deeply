121c2149bbf310ef2711d914539c8542b380606d
brian <brian.mina17@gmail.com>
2024-01-23 20:38:51 -0500

    continuie

--- functions/printing_models.py
+++ functions/printing_models.py
@@ -1,17 +1,6 @@
-
-
+from printing_functions import print_models
 # Simulate printing each design, until none are left.
 # Move each design to completed_models after printing.
-
-
-def print_models(unprinted_designs, completed_models):
-    """Simulate printing each design, until none are left
-    Move each design to completed_models after printing."""
-
-    while unprinted_designs:
-        current_design = unprinted_designs.pop()
-        print(f"Printing model: {current_design}")
-        completed_models.append(current_design)
 
 
 def show_completed_models(completed_models):
@@ -22,7 +11,7 @@ def show_completed_models(completed_models):
 
 
 unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
-completed_models = []
+complete = []
 
 print_models(unprinted_designs, completed_models)
 show_completed_models(completed_models)
