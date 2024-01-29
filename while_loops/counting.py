6c61da9baf5abc7821295df0c8128e71ef634745
brian <brian.mina17@gmail.com>
2024-01-29 16:39:08 -0500

    statement "continue" means STOP and start again if this was TRUE

--- while_loops/counting.py
+++ while_loops/counting.py
@@ -2,7 +2,8 @@
 current_number = 0
 
 while current_number < 10:
-
-
     current_number += 1  # this is a shorthand for current_number = current_number + 1
+    if current_number % 2 == 0:
+        continue
 
+    print(current_number)
