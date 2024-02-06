1364fd226499b0c6b9bd16011f3f37aefbccb8c5
brian <brian.mina17@gmail.com>
2024-02-06 16:29:02 -0500

    files and exceptions practice chapther 10

--- weather_data/sitka_highs_lows.py
+++ weather_data/sitka_highs_lows.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 import matplotlib.pyplot as plt
 
-path = Path('sitka_weather_07-2021_simple.csv')
+path = Path('death_valley_2021_simple.csv')
 lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
@@ -14,21 +14,28 @@ print(header_row)
 for index, column_header in enumerate(header_row):
     print(index, column_header)
 
-# Extract high temperatures.
-highs = []
+# Extract high, low and date temperatures.
+dates, highs, lows = [], [], []
 for row in reader:
+    low = int(row[5])
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 print(highs)
 
 # Plot the high temperatures.
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='green')
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color="blue", alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor="blue", alpha=0.1)
 
-ax.set_title("Daily High Temperature, July 2021", fontsize=24)
+ax.set_title("Daily High And Low Temperature, 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
