2ec0da521d479586ca3e4fb82eec1a412ad736d1
brian <brian.mina17@gmail.com>
2024-02-07 21:41:15 -0500

    Reading files with Pathlib

--- files_exceptions/file_reader.py
+++ files_exceptions/file_reader.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 
 path = Path("pi_digits.txt")
-content = path.read_text()
+contents = path.read_text()
+# contents = contents.rstrip()
 
-print(content)
+lines = contents.splitlines()
+for line in lines:
+    print(line)
