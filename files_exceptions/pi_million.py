0b7ee62dd5a9b4364515444cf6e2f8c44fbc0ca3
brian <brian.mina17@gmail.com>
2024-02-09 09:46:32 -0500

    using replace() method in a for loop after reading the entire file as string

---
+++ files_exceptions/pi_million.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+
+path = Path("pi_million_digits.txt")
+contents = path.read_text()
+
+lines = contents.splitlines()
+pi_string = ""
+for line in lines:
+    pi_string += line
+
+print(f"{pi_string[:52]}...")
+print(len(pi_string))
+
+
+birthday = input("Enter your birthday, i the form mmddyy")
+if birthday in pi_string:
+    print("Your Birthday appears in the first millon of pi!")
+else:
+    print("sooooryyyyy!")
